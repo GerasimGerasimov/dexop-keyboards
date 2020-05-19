41f8b4c8b225fd2790fb0139f20949f0ccc35127
Gerasim Gerasimov <dialix@yandex.ru>
2020-05-19 16:11:35 +0700

    работают OK, Cancel и Enter на поле ввода значения. Дальше делать кнопки 0..9, DEL, BK, R

--- src/components/UI/KeyBoards/KeyBoardNumeric/KeyBoardNumeric.tsx
+++ src/components/UI/KeyBoards/KeyBoardNumeric/KeyBoardNumeric.tsx
@@ -2,27 +2,74 @@ import React, {Component} from 'react'
 import {IKeyBoardProps } from '../IKeyBoards';
 import './KeyBoardNumeric.css'
 
-export default class KeyBoardNumeric extends Component<IKeyBoardProps, {}> {
+interface IState {
+  value: string;
+}
 
+export default class KeyBoardNumeric extends Component<IKeyBoardProps, IState> {
+
+  private prevValue: string = this.props.data.value.toString();
+  
   constructor (props: any){
     super(props)
+    this.state = {
+      value:  this.prevValue
+    };
   }
 
-  handleHide(e: any) {
-    this.props.onClick(e);
+  private handleHide(cause: string) {
+    const values: any = {
+      'ok': this.state.value,
+      'cancel':this.prevValue
+    }
+    const result: any = {
+      cause,
+      value: values[cause]
+    }
+    this.props.onClick(result);
+  }
+
+  private inputChangedHandler = (event: any) => {
+    const value = event.target.value;
+    this.setState({value})
+    console.log(this.state.value)
+  }
+
+  private Enter(event: any) {
+    if(event.key === 'Enter'){
+      this.handleHide('ok')
+    }
   }
 
   render() {
     return(
-      <div>
-        <h1>KeyBoardNumeric</h1>
-          <button onClick={(e)=>this.handleHide(e)}>Hide modal</button>
+      <div className="KeyBoardBlock">
           <div className="kbn-grid-container">
-            <div className="kbn-header">Iexc: 100.501013</div>
+            <div className="kbn-header">
+              {`${this.props.data.name}:${this.prevValue}`}
+            </div>
             <div className="kbn-discription">Ток возбуждения</div>
-            <div className="kbn-value">100.501013</div>
-            <div className="kbn-ok">Ok</div>
-            <div className="kbn-cancel">Cancel</div>
+            <div className="kbn-value">
+              <input
+                  type="text"
+                  className="KeyBoardText"
+                  onChange={(event)=>this.inputChangedHandler(event)}
+                  onKeyDown={(event)=>this.Enter(event)}
+                  value = {this.state.value}>
+                </input>
+            </div>
+            <div className="kbn-ok">
+              <button 
+                  className="KeyBoardButton"
+                  onClick={()=>this.handleHide('ok')}>Ok
+                </button>
+            </div>
+            <div className="kbn-cancel">
+              <button
+                className="KeyBoardButton"
+                onClick={()=>this.handleHide('cancel')}>Chancel
+              </button>
+            </div>
             <div className="kbn-del">DEL</div>
             <div className="kbn-backspace">←</div>
             <div className="kbn-dot">.</div>
