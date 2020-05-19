bc23d2587a6b02c3046c2dc71181a70c269e5b0e
Gerasim Gerasimov <dialix@yandex.ru>
2020-05-19 08:20:50 +0700

    сделал CSS разметку KeyBoardNimeric

--- src/components/UI/KeyBoards/KeyBoardNumeric/KeyBoardNumeric.tsx
+++ src/components/UI/KeyBoards/KeyBoardNumeric/KeyBoardNumeric.tsx
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import {IKeyBoardProps } from '../IKeyBoards';
+import './KeyBoardNumeric.css'
 
 export default class KeyBoardNumeric extends Component<IKeyBoardProps, {}> {
 
@@ -16,6 +17,27 @@ export default class KeyBoardNumeric extends Component<IKeyBoardProps, {}> {
       <div>
         <h1>KeyBoardNumeric</h1>
           <button onClick={(e)=>this.handleHide(e)}>Hide modal</button>
+          <div className="kbn-grid-container">
+            <div className="kbn-header">Iexc: 100.501013</div>
+            <div className="kbn-discription">Ток возбуждения</div>
+            <div className="kbn-value">100.501013</div>
+            <div className="kbn-ok">Ok</div>
+            <div className="kbn-cancel">Cancel</div>
+            <div className="kbn-del">DEL</div>
+            <div className="kbn-backspace">←</div>
+            <div className="kbn-dot">.</div>
+            <div className="kbn-return">R</div>
+            <div className="kbn-n0">0</div>
+            <div className="kbn-n1">1</div>
+            <div className="kbn-n2">2</div>
+            <div className="kbn-n3">3</div>
+            <div className="kbn-n4">4</div>
+            <div className="kbn-n5">5</div>
+            <div className="kbn-n6">6</div>
+            <div className="kbn-n7">7</div>
+            <div className="kbn-n8">8</div>
+            <div className="kbn-n9">9</div>
+          </div>
       </div>
     )
   }
